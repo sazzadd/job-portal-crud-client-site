56a282959cb6102372de66dc713a1ed638aa7f3f
sazzad <sazzadador@gmail.com>
2024-12-17 11:55:40 +0600

    add axios interceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,23 @@
+import axios from "axios";
+import { useEffect } from "react";
 
-
-const instance = axios.create({
-    baseURL: "http://localhost:5000",
-    WithCredentials: true,
-  });
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5000",
+  WithCredentials: true,
+});
 const useAxiosSecure = () => {
- 
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error caught interceptor", error);
+        return Promise.reject(error);
+      }
+    );
+  });
+  return axiosInstance;
 };
 
 export default useAxiosSecure;
--- src/pages/MyApplications.jsx
+++ src/pages/MyApplications.jsx
@@ -1,18 +1,20 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
+import useAxiosSecure from "../hooks/useAxiosSecure";
 
 const MyApplications = () => {
   const { user } = useAuth();
   const [jobs, setJobs] = useState([]);
+  const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
-    // fetch(`http://localhost:5000/job-applications?email=${user.email}`)
-    //   .then((res) => res.json())
-    //   .then((data) => setJobs(data));
-    axios
-      .get(`http://localhost:5000/job-applications?email=${user.email}`,{withCredentials:true})
-      .then((res) => console.log(setJobs(res.data)));
+    // axios
+    //   .get(`http://localhost:5000/job-applications?email=${user.email}`,{withCredentials:true})
+    //   .then((res) => console.log(setJobs(res.data)));
+
+    axiosSecure
+      .get(`/job-applications?email=${user.email}`)
+      .then((res) => setJobs(res.data));
   }, [user.email]);
 
   return (
